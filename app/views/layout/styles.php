b4da3731c25e6b213faa8f1b115346fd572bb282
AguineleQueiroz <aguinele.queiroz@gafit.com.br>
2025-01-31 16:41:43 -0300

    styles: adds styles of the base templates

--- app/views/layout/styles.php
+++ app/views/layout/styles.php
@@ -2,4 +2,5 @@
 
 <link rel="stylesheet" href="/assets/css/main.css">
 <link rel="stylesheet" href="/assets/css/partials/header.css">
-<link rel="stylesheet" href="/assets/css/partials/footer.css">
+<link rel="stylesheet" href="/assets/css/partials/header-logged.css">
+<link rel="stylesheet" href="/assets/css/partials/footer-default.css">
